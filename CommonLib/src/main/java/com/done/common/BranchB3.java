d8e8283a9781b1b5c439f8148f38f2e995470037
maruilong <maruilong@kuaishou.com>
2021-04-15 15:19:39 +0800

    commit branch branchB3

---
+++ CommonLib/src/main/java/com/done/common/BranchB3.java
@@ -0,0 +1,12 @@
+package com.done.common;
+
+/**
+ * Class: com.done.common.BranchB3
+ * Des: This is ....
+ * <p>
+ * author: maruilong
+ * date: 2021/4/15
+ * mail: maruilong@kuaishou.com
+ **/
+public class BranchB3 {
+}
