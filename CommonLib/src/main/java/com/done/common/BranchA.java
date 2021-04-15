212ea5743e55ca9d126a7b43a60749366831c60f
maruilong <maruilong@kuaishou.com>
2021-04-15 14:21:07 +0800

    commit branch a

---
+++ CommonLib/src/main/java/com/done/common/BranchA.java
@@ -0,0 +1,12 @@
+package com.done.common;
+
+/**
+ * Class: com.done.common.BranchA
+ * Des: This is ....
+ * <p>
+ * author: maruilong
+ * date: 2021/4/15
+ * mail: maruilong@kuaishou.com
+ **/
+public class BranchA {
+}
