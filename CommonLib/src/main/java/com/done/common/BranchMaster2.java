5d5863cfa0fb24e9999a1abe3e12aafae787b51e
maruilong <maruilong@kuaishou.com>
2021-04-15 15:20:21 +0800

    commit branch master2

---
+++ CommonLib/src/main/java/com/done/common/BranchMaster2.java
@@ -0,0 +1,12 @@
+package com.done.common;
+
+/**
+ * Class: com.done.common.BranchMaster2
+ * Des: This is ....
+ * <p>
+ * author: maruilong
+ * date: 2021/4/15
+ * mail: maruilong@kuaishou.com
+ **/
+public class BranchMaster2 {
+}
