5867ea9accf73e0d7d33c1fce3c4fb04deec0b2d
maruilong <maruilong@kuaishou.com>
2021-05-25 21:51:42 +0800

    commit branch master2

---
+++ CommonLib/src/main/java/com/done/common/NewMaster.java
@@ -0,0 +1,13 @@
+package com.done.common;
+
+/**
+ * Class: com.done.common.NewMaster
+ * Des: This is ....
+ * <p>
+ * author: maruilong
+ * date: 2021/5/25
+ * mail: maruilong@kuaishou.com
+ **/
+public class NewMaster {
+    //Master-1
+}
